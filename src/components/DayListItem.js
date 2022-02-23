4b9b91e4d3d1a577d5cbee02994c66cfb334c76f
Chris Hatcher <hatcherchris20@gmail.com>
2022-02-23 14:38:35 -0800

    adding files after fixing installation bug

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  
+  const dayClass = classNames("day-list__item", {
+    "day-list_item--selected": props.selected,
+    "day-list_item--full": props.spots === 0
+  })
+
+  const formatSpots = function(spotCount) {
+    if (spotCount === 0) {
+      return "no spots remaining"
+    } else if (spotCount === 1) {
+      return "1 spot remaining"
+    } else {
+      return `${spotCount} spots remaining`
+    }
+  }
+  
+  return (
+    // <li> represents the entire day item
+    <li
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)} </h3>
+    </li>
+  );
+}
+
+
+
