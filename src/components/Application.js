9dbcaf360f053e3d174f9ecef93da7658bc7a104
Chris Hatcher <hatcherchris20@gmail.com>
2022-02-27 18:33:42 -0800

    adding helper functions and custom hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,87 +1,123 @@
-// Since <Application> and <DayList> will both need to know the value of day, we should track day in <Application> (the closest common ancestor of both components).
-// We stored the day state in the <Application> component.
-// The main reason we are not storing the day state in the <DayList> is that we need to use that state with other components. By storing it in <Application>, we can 
-// easily pass day down via props to any component that needs it.
-// When we call the setDay action, it changes the day state. When we change the state, the <Application> renders and passes the new day to the <DayList>. The <DayList> 
-// renders and passes props to the <DayListItem> children causing the updates to the selected visual state.
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "components/DayList.js";
 import "components/Application.scss";
 import Appointment from "./Appointment";
+import axios from "axios";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// days and appointments
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 
-
-
-
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//   }
+// ];
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");      // Adding state to Application.js
-  console.log("The current state is (day): ", day);      // console.log the state
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      const [days, appointments, interviewers] = all;
+        console.log(days);
+        console.log(appointments);
+        console.log(interviewers);
+      setState(prev => ({
+        ...prev,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+        }))
+      })
+    }, [])
+
+  
+  
+  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  
+
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview)
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    )
+  })
+
 
   return (
     <main className="layout">
 
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
 
         <img
           className="sidebar--centered"
@@ -91,8 +127,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
@@ -106,11 +142,8 @@ export default function Application(props) {
 
       <section className="schedule">
 
-        {appointments.map((appointment) =>
-          // <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
-          <Appointment key={appointment.id} {...appointment} />
-          )}
-          
+        {schedule}
+
         <Appointment key="last" time="5pm" />
 
       </section>
@@ -121,15 +154,3 @@ export default function Application(props) {
 
 
 
-
-
-
-// const days = props.days.map(day =>
-//   <DayListItem
-//     key={props.id}
-//     name={props.name}
-//     spots={props.spots}
-//     selected={props.name === props.value}
-//     setDay={props.onChange}
-//   />
-// );
---
+++ src/helpers/selectors.js
@@ -0,0 +1,44 @@
+export function getAppointmentsForDay(state, day) {
+
+  //... returns an array of appointments for that day
+
+  const filteredDays = state.days.find(x => x.name === day)
+  // console.log("filteredDays", filteredDays)
+
+  const emptyArray = [];
+
+  if (filteredDays) {
+    const y = filteredDays.appointments.map(id => state.appointments[id]);
+    // console.log("y = ", y);
+    return y;
+  } else {
+    return emptyArray;
+  }
+
+}
+
+
+
+
+
+export function getInterview(state, interview) {
+
+  console.log("state = ", state);
+  console.log("interview (before) = ", interview);
+
+  if (state.interviewers && interview) {
+    interview = {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer],
+    };
+    console.log("interview (after) = ", interview);
+    return interview;
+  } else {
+    return null;
+  }
+
+  // We found that we need to transform an interview object 
+  // with an id representing the interviewer to an object 
+  // containing a nested object.
+
+}
