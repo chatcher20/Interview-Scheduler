758683484cf65993e78be7f0571e67662852e106
Chris Hatcher <hatcherchris20@gmail.com>
2022-03-01 21:43:26 -0800

    adding spots left update functionality

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -33,6 +33,27 @@ export default function useApplicationData() {
     })
   }, [])
 
+  // We should update the spots when we book or cancel an interview. It should be done in the bookInterview 
+  // and cancelInterview functions, and applied in the .then part of the AJAX request.
+  // Note: The appointment id is known when an interview is confirmed or canceled by the server.
+  const availableSpots = function (today, day, increment) {
+
+    let spot = day.spots;
+
+    // If an interview is booked (aka a new appointment therefore appointment id is created), the available spots will decrease
+    if (today === day.name && !increment) {
+      return spot -= 1
+    }
+    // If an interview is deleted, the available spots will increase/increment by 1
+    else if (today === day.name && increment === true) {
+      return spot += 1
+    }
+    // If an interview is edited (but not created or deleted therefore no change in spots available)
+    else {
+      return spot
+    }
+  };
+
   // The bookInterview action makes an HTTP request and updates the local state.
   const bookInterview = function (id, interview) {
     console.log(id, interview);
@@ -47,12 +68,13 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const days = state.days.map((day) => {return {...day, spots: availableSpots(state.day, day, false)}});
+
     return axios
       .put(`/api/appointments/${id}`, { interview })
       .then((response) => {
-        setState({ ...state, appointments });
+        setState({ ...state, appointments, days });    
       })
-
   };
 
   // The cancelInterview action makes an HTTP request and updates the local state.
@@ -68,14 +90,14 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const days = state.days.map((day) => {return {...day, spots: availableSpots(state.day, day, true)}});
+
     return axios
       .delete(`/api/appointments/${id}`)
       .then((response) => {
-        setState({ ...state, appointments })
+        setState({ ...state, appointments, days });
       })
-
-  }
-
+  };
 
   return { state, setDay, cancelInterview, bookInterview };
-}
+};
